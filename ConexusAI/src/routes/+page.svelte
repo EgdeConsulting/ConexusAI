c4a41bd45cda5d26a2441d7cd538da2b7b654c55
TheWhipAbdel <Abdel.Sadaqi@egde.no>
2024-02-01 13:06:38 +0100

    Create svelte project, components fil and chatbotInterface component.

---
+++ ConexusAI/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import {ChatbotInterface as Chatbot, NavbarSet} from "$lib";
+</script>
+
+<main
+  style="display: flex; align-items: center; justify-content: center; height: 100vh;"
+>
+  <Chatbot />
+</main>
