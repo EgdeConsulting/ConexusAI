c3fa7bad67a17b956e51732efba970009d82cd2a
TheWhipAbdel <Abdel.Sadaqi@egde.no>
2024-02-01 15:47:43 +0100

    Make it a NavbarButtons.svelte and have to work with style

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,20 @@
 <script>
-  import { ChatbotInterface as Chatbot, NavbarSet, NavbarButtons } from "$lib";
+  import { ChatbotInterface, NavbarButtons, ShareChatIcon } from "$lib";
+
+  function test() {
+    alert("test method");
+  }
+  const NavbarButtonsArray = [
+    { picture: ShareChatIcon, text: "hei", method: test },
+  ];
 </script>
 
 <main
   style="display: flex; align-items: center; justify-content: center; height: 100vh;"
 >
-  <Chatbot />
+  <ChatbotInterface />
+  <div>
+    
+    <NavbarButtons {NavbarButtonsArray} />
+  </div>
 </main>
