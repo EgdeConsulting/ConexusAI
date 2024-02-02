6dc75ebec1cf3df25df43c144fab5dfef3ee233e
TheWhipAbdel <Abdel.Sadaqi@egde.no>
2024-02-02 09:29:23 +0100

    finish the NavbarButtons

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,16 +5,32 @@
     alert("test method");
   }
   const NavbarButtonsArray = [
-    { picture: ShareChatIcon, text: "hei", method: test },
+    { picture: ShareChatIcon, text: "text 1", method: test },
+    { picture: ShareChatIcon, text: "text 2", method: test },
   ];
 </script>
 
-<main
+<nav
   style="display: flex; align-items: center; justify-content: center; height: 100vh;"
 >
-  <ChatbotInterface />
-  <div>
-    
+  <section class="container">
     <NavbarButtons {NavbarButtonsArray} />
-  </div>
-</main>
+  </section>
+  <aside><ChatbotInterface /></aside>
+</nav>
+<nav></nav>
+
+<style>
+  .container {
+    border-top: 1px solid #333;
+    border-bottom: 1px solid #333;
+    min-height: 200px;
+    height: calc(100vh - 15em);
+    width: 20%;
+  }
+  aside {
+    /* Juster bredden etter behov */
+    width: 80%; /* Eksempel: Gjør chatbot-interface 80% av skjermens bredde */
+    padding: 1em; /* Legger til litt padding rundt chatbot-interface for estetikk */
+  }
+</style>
