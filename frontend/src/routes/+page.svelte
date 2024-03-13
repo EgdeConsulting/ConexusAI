391f055057b37b47290cb56c454bb0dbae0277a7
TheWhipAbdel <Abdel.Sadaqi@egde.no>
2024-03-13 17:00:30 +0100

    Create a ExpandedButton.svelte componant

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,27 +1,73 @@
 <script>
-  import { ChatbotInterface, NavbarSet, SendIcon } from "$lib";
+  import { ChatbotInterface, NavbarSet, SendIcon, ExpandedButton } from "$lib";
 
   function test() {
     alert("test method");
   }
-  const NavbarButtonsArray = [
-    { icon: SendIcon, text: "text 1", method: test },
-    { icon: SendIcon, text: "text 2", method: test },
-    { icon: SendIcon, text: "text 3", method: test },
+  // const NavbarButtonsArray = [
+  //   { icon: SendIcon, text: "text 1", method: test },
+  //   { icon: SendIcon, text: "text 2", method: test },
+  //   { icon: SendIcon, text: "text 3", method: test },
+  // ];
+  let buttons = [
+    {
+      id: "LagGraf",
+      icon: SendIcon,
+      text: "Lag graf...",
+      subButtons: [
+        { text: "Linjediagram", method: test, icon: SendIcon },
+        { text: "Søylediagram", method: test, icon: SendIcon },
+        { text: "Kakediagram", method: test, icon: SendIcon },
+      ],
+    },
+    {
+      id: "download",
+      icon: SendIcon,
+      text: "Download",
+      subButtons: [
+        { text: "Download as PDF", method: test, icon: SendIcon },
+        { text: "Download as Excel", method: test, icon: SendIcon },
+      ],
+    },
+    {
+      id: "send",
+      icon: SendIcon,
+      text: "Send",
+      subButtons: [
+        { text: "Send as PDF", method: test, icon: SendIcon },
+        { text: "Send as Excel", method: test, icon: SendIcon },
+      ],
+    },
+    {
+      id: "send",
+      icon: SendIcon,
+      text: "Del chat...",
+      method: test,
+    },
+    {
+      id: "send",
+      icon: SendIcon,
+      text: "hjelp...",
+      method: test,
+    },
   ];
   let showNavbar = true;
 </script>
 
 <div class="main-container">
   <section class="navbar-container">
-    <NavbarSet {NavbarButtonsArray} {showNavbar} />
+    <NavbarSet {buttons} {showNavbar} />
+    <!-- <ExpandedButton {buttons} /> -->
   </section>
-  <aside class="chat-interface">
+  <aside class="chat-interface {showNavbar ? '' : 'full-width'}">
     <ChatbotInterface />
   </aside>
 </div>
 
 <style>
+  div {
+    font-family: "Open Sans";
+  }
   .main-container {
     display: flex;
     height: 100vh; /* Fyller hele skjermens høyde */
@@ -35,8 +81,12 @@
     height: auto;
   }
   .chat-interface {
-    flex-grow: 10; /* Tar opp resterende plass */
+    flex-grow: 1; /* Tar opp resterende plass */
     background-color: #f0f0f0; /* Eksempel på bakgrunnsfarge */
     height: auto;
+  }
+  .chat-interface.full-width {
+    width: 100%; /* Tar opp hele bredden når navbar ikke vises */
+    position: center;
   }
 </style>
