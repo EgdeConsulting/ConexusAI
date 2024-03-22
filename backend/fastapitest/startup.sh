9233964c7db539cff51582bfb09a3ee627eb7975
Kols1 <kim.olsen@egde.no>
2024-03-22 13:09:52 +0100

    24 Added startup.sh

---
+++ backend/fastapitest/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the root directory of the application
+cd $HOME/site/wwwroot
+
+# Activate the Python virtual environment (if applicable)
+source /path/to/venv/bin/activate
+
+# Run the Python program
+python main.py
