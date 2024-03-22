937a984355cff0001f48ea219af620c77dcf49c6
DrEgde <daniel.rasmussen@egde.no>
2024-03-22 14:41:12 +0100

    #25 finished connectingtodatabase

--- backend/connect-to-database/25-connect-to-database.py
+++ backend/connect-to-database/25-connect-to-database.py
@@ -11,15 +11,14 @@ client = OpenAI(api_key = APIkey)
 conn = pyodbc.connect(connection_string)
 cursor = conn.cursor()
 
-"""
 # Execute the SELECT query
 cursor.execute('SELECT * FROM fact_barnehage_nokkeltall')
 
 # Fetch and print the results
 for row in cursor.fetchall():
     print(row)
-"""
 
+"""
 # Ask a question using OpenAI's API
 user_query = "Hvor mange barnehager er det i Norge?"
 chat_completion = client.chat.completions.create(model="gpt-4-turbo-preview",
@@ -30,14 +29,17 @@ chat_completion = client.chat.completions.create(model="gpt-4-turbo-preview",
 # Extract the response from OpenAI
 answer = chat_completion.choices[0].message.content
 
+
 # Execute a generic query to search the entire database
 query = f"SELECT * FROM asql-processed-prod-dw-2024-2-13-17-9 WHERE question LIKE {answer}"
 cursor.execute(query)
 
+
 # Fetch and print the results
 for row in cursor.fetchall():
     print(row)
-
+"""
+    
 # Close the cursor and connection
 cursor.close()
 conn.close()
