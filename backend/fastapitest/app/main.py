ad51d8127f0b54e22465147dd7671540e3439e97
Kols1 <kim.olsen@egde.no>
2024-04-05 13:10:18 +0200

    24 updated functions

--- backend/fastapitest/app/main.py
+++ backend/fastapitest/app/main.py
@@ -6,7 +6,17 @@ app = FastAPI()
 @app.get("/")
 async def read_item():
     return {"Hello": "World"}   
+
 @app.get("/hello/{name}")
 async def read_item(name):
     return {"Hello": name}
+
+@app.get("/query/{query}")
+async def read_item(query):
+    return {"Query": query}
+
+@app.get("/reverse")
+async def reverse_string(query_param: str):
+    reversed_string = query_param[::-1]
+    return {"reversed_string": reversed_string}
 
