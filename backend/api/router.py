fda56323a0c15e2bd025a8b0a67db39586ff9777
Kols1 <kim.olsen@egde.no>
2024-04-11 14:21:44 +0200

    Add API endpoint for receiving messages in router.py

--- backend/api/router.py
+++ backend/api/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from service.testfunc import addition
 
 
@@ -13,10 +13,17 @@ async def read_root():
 
 @router.get("/query/{input_string}")
 async def read_item(input_string: str):
-
     return {"input_string": input_string}
 
-@router.get("/test/{input_int1}{input_int2}")
+@router.post("/message/")
+async def read_item(request: Request):
+    data = await request.json()
+    USER_INPUT = data.get("prompt")
+    #function here! 
+    #answer = result from query
+    return { "answer": "placeholder" }
+
+@router.get("/test/")
 async def test(input_int1: int, input_int2: int):
     result =addition(input_int1, input_int2)
     return {"result": result}   
