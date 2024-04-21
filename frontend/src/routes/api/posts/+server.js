92e9be6e228b2c574b53fb91592176b00092151e
TheWhipAbdel <Abdel.Sadaqi@egde.no>
2024-04-21 13:30:26 +0200

    #39 Update API endpoint in GET method of server.js and fix styling issue in NavbarSet component

--- frontend/src/routes/api/posts/+server.js
+++ frontend/src/routes/api/posts/+server.js
@@ -1,3 +1,14 @@
+export const GET = async ({ request }) => {
+  const autherHeader = request.headers.get("Authorization");
+
+  // if (autherHeader !== 'ConexusAI') {
+  //     return new Response(JSON.stringify({message:'Unauthorized: invalid credentials'}), { status: 401 });
+  // }
+  const res = await fetch("https://python-chat-api.onrender.com/python/prompt");
+  const data = await res.json();
+
+  return new Response(JSON.stringify({ data }), { status: 200 });
+};
 export const POST = async ({ request }) => {
   try {
     const { prompt } = await request.json(); // forespørselskroppen inneholder en "prompt"
@@ -16,7 +27,7 @@ export const POST = async ({ request }) => {
       throw new Error(`API call failed with status ${apiResponse.status}`);
     }
 
-    const data = await apiResponse.json(); // Anta at API-et returnerer et objekt som inneholder "answer"
+    const data = await apiResponse.json(); // API-svaret inneholder et "answer"
 
     // Send svaret tilbake til klienten
     return new Response(JSON.stringify({ answer: data.answer }), {
@@ -28,15 +39,3 @@ export const POST = async ({ request }) => {
     });
   }
 };
-
-// export async function POST({ request }) {
-//     // Hent meldingsteksten fra forespørselskroppen
-//     const data = await request.json();
-//     const newMessage = {
-//         id: messages.length + 1,
-//         text: data.text,
-//     };
-//     messages.push(newMessage);
-//     // Returner den nye meldingen med statuskode 201 (Created)
-//     return json(newMessage, { status: 201 });
-// }
