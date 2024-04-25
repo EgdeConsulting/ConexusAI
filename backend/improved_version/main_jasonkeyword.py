e64ce42b044eeecc6e860160c2b48137773f7068
DrEgde <daniel.rasmussen@egde.no>
2024-04-25 14:31:18 +0200

    #71ferdigfordagen

--- backend/improved_version/main_jasonkeyword.py
+++ backend/improved_version/main_jasonkeyword.py
@@ -10,31 +10,31 @@ conn_string = 'mssql+pyodbc:///?odbc_connect=' + connection_string
 engine = create_engine(conn_string)
 
 try:
-	# Function to retrieve data from the dim_indikator table
-	def get_dim_indikator_data(keyword):
-		query = """
-			SELECT [navn], [maaleenhet], [human_readable_table_name], [eier], [eierbeskrivelse], [eierSistEndret], [fraDato], [tilDato], [fagomraade], [dim_indikator_key], [eierbeskrivelseURL], [aktiv]
-			FROM [dbo].[dim_indikator]
-			WHERE navn LIKE :keyword
-			   OR eierbeskrivelse LIKE :keyword
-			   OR human_readable_table_name LIKE :keyword
-			   OR fagomraade LIKE :keyword
-		"""
-		with engine.connect() as conn:
-			#result = conn.execute(text(query), {'keyword': f"%{keyword}%"})
-			result = conn.execute(text(query), [("keyword", f"%{keyword}%")])
-			return [dict(row) for row in result]
+    # Function to retrieve data from the dim_indikator table
+    def get_dim_indikator_data(keyword):
+        query = """
+            SELECT [navn], [maaleenhet], [human_readable_table_name], [eier], [eierbeskrivelse], [eierSistEndret], [fraDato], [tilDato], [fagomraade], [dim_indikator_key], [eierbeskrivelseURL], [aktiv]
+            FROM [dbo].[dim_indikator]
+            WHERE navn LIKE '%keyword%'
+               OR eierbeskrivelse LIKE :keyword
+               OR human_readable_table_name LIKE :keyword
+               OR fagomraade LIKE :keyword
+        """
+        with engine.connect() as conn:
+            result = conn.execute(text(query), keyword=f"%{keyword}%")
+            #result = conn.execute(text(query), {'keyword': f"%{keyword}%"})
+            return [dict(row) for row in result]
 
-	# Example usage
-	keyword = 'barnehagelærer'
-	data = get_dim_indikator_data(keyword)
-	print(data)
+    # Example usage
+    keyword = 'barnehagelærer'
+    data = get_dim_indikator_data(keyword)
+    print(data)
 
-	# Write the data to a JSON file
-	with open("dim_indikator_data.json", "w") as file:
-		json.dump(data, file, indent=2)
+    # Write the data to a JSON file
+    with open("dim_indikator_data.json", "w") as file:
+        json.dump(data, file, indent=2)
 
 except Exception as e:
-	print(f"An error occurred: {str(e)}")
+    print(f"An error occurred: {str(e)}")
 
 
