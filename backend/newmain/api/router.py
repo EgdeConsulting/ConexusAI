9c2a3e7cb5ae4bc19796457a84b1ecce76c66cc1
DrEgde <daniel.rasmussen@egde.no>
2024-04-26 13:37:41 +0200

    #79 working-backend-wohoo

---
+++ backend/newmain/api/router.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Request, HTTPException
+from fastapi.responses import JSONResponse
+from test_old_version_new_query_final_a_only import get_input_from_frontend
+
+
+router = APIRouter()
+
+
+@router.get("/")
+async def read_root():
+    message = "ConexisAI"
+    docs_url = "/docs'>"
+    return {"message": message}
+
+@router.post("/message/")
+async def read_item(request: Request):
+    data = await request.json()
+    query = data.get("prompt")
+    answer = get_input_from_frontend(query)
+    return { "output": answer}
+
---
+++ backend/newmain/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from api.router import router
+
+
+app = FastAPI()
+
+
+app.include_router(router)
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("main:app",  host="0.0.0.0", port=80, reload=True)
