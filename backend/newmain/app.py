42d2ff114ec5e03b54decafdfa210d38323da038
Kols1 <kim.olsen@egde.no>
2024-04-29 10:16:43 +0200

    Update database connection string in app.py and test_old_version_new_query_final_a_only.py

--- backend/newmain/app.py
+++ backend/newmain/app.py
@@ -6,7 +6,10 @@ load_dotenv()
 # Define the connection string
 server = os.getenv('server')
 database = os.getenv('database')
-username = os.getenv('username')
+username = os.getenv('loginname')
+
 password = os.getenv('password')
 driver = os.getenv('driver')
+
+
 connection_string = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
